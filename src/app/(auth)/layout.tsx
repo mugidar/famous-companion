286c3019fbacd87779984a1cab589dee3b08a924
mugidar <mugidar@gmail.com>
2023-08-19 12:36:58 +0300

    clerk setup

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface AuthProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout: React.FC<AuthProps> = ({ children }) => {
+  return (
+    <div className="h-screen bg-slate-200 flex items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
